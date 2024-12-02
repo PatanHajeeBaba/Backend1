f44e009d5eefcb4c44af8ae17f47506f657ba6dd
PatanHajeeBaba <hajeepatan046@gmail.com>
2024-12-02 21:36:39 +0530

    Update LoginForm.js

--- Backend/Models/LoginForm.js
+++ Backend/Models/LoginForm.js
@@ -2,46 +2,10 @@ const mongoose = require('mongoose')
 const schema = mongoose.Schema
 
 let UserSchema = new schema({
-    fname:{
+    Email:{
         type:String,
     },
-    lname:{
-        type:String,
-    },
-    gender:{
-        type:String,
-    },
-    phone:{
-        type:Number,
-    },
-    dob:{
-        type:String,
-    },
-    email:{
-        type:String,
-    },
-    address:{
-        type:String,
-    },
-    street:{
-        type:String,
-    },
-    city:{
-        type:String,
-    },
-    zip:{
-        type:String,
-    },
-    highschool:{
-        type:String,
-    },
-    university:{
-        type:String,
-    },
-    experience1:{
-        type:String,
-    },
-    experience2:{
+    Password:{
         type:String,
     },
 })
